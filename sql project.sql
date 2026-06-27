275d840bf52287be9a8ff6f4e4354adbaa3176e3
Uttam Kumar <140340735+uttamkr06@users.noreply.github.com>
2026-06-27 16:45:03 +0530

    Add files via upload

---
+++ sql project.sql
@@ -0,0 +1,14 @@
+-- 1) Retrieve all books in the "Fiction" genre:
+SELECT *
+FROM Books 
+WHERE Genre = 'Fiction';
+
+-- 2) Find books published after the year 1950:
+SELECT * 
+FROM Books
+WHERE Published_year > 1950;
+
+-- 3) List all customers from the Canada:
+
+
+-- 4) Show orders placed in November 2023:
